f5ec209dbdacb893f47cf2bd281ae35bdb610711
LuK1337 <priv.luk@gmail.com>
2018-08-12 16:10:01 +0200

    msm8996-common: Treble sucks
    Change-Id: I304cbfbfba8c7a4c403a8e865bf0d940cac1bea2

--- extract-files.sh
+++ extract-files.sh
@@ -24,7 +24,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/hash/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -63,5 +63,9 @@ if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
 
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
 fi
+
+COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+
+patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
 
 "$MY_DIR"/setup-makefiles.sh
